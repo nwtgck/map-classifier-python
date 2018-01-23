106c6b231a9e9c241a5fe30c6a6ef4c14d745097
Ryo Ota <nwtgck@gmail.com>
2018-01-23 23:57:22 +0900

    [remove/test] Remove unused codes and Add a test

--- map_classifier/__init__.py
+++ map_classifier/__init__.py
@@ -3,23 +3,6 @@
 from collections import defaultdict
 import numpy as np
 
-def _bayesian_map_pred(p_j_x_Ci_dict, x_j_dict, p_j_x_dict, j_list, Ci_list):
-    def _1(Ci, j):
-        x = x_j_dict[j]
-        return p_j_x_Ci_dict[j][x][Ci] / p_j_x_dict[j][x]
-
-    return list(map(
-        lambda Ci:
-        np.product(list(map(lambda j: _1(Ci, j), j_list))),
-        Ci_list
-    ))
-
-
-def bayesian_map_pred(p_j_x_Ci_dict, x_j_dict, p_j_x_dict, j_list, Ci_list):
-    _pred = _bayesian_map_pred(p_j_x_Ci_dict, x_j_dict, p_j_x_dict, j_list, Ci_list)
-    return max(zip(_pred, Ci_list), key=lambda p_and_Ci: p_and_Ci[0])[1]
-
-
 class MAPClassifier:
     def __init__(self):
         pass
@@ -37,8 +20,6 @@ class MAPClassifier:
             for j, x in enumerate(feature):
                 self.p_j_x_dict[j][x] += 1
                 self.p_j_x_Ci_dict[j][x][Ci] += 1
-
-        print("FIT!")
 
     def _predict(self, X):
         _preds = []
@@ -65,10 +46,6 @@ class MAPClassifier:
             x = feature[j]
             numerator   = p_j_x_Ci_dict[j][x][Ci]
             denominator = p_j_x_dict[j][x]
-            # if denominator != 0:
-            #     print("NOT ZERO~~~~~~~")
-            # print("denominator:", denominator)
-            # return numerator / denominator
             return 0 if denominator == 0 else numerator / denominator # TODO 0 is OK?
 
         return list(map(
--- tests/map_classifier_test.py
+++ tests/map_classifier_test.py
@@ -1,11 +1,21 @@
 import unittest
+from sklearn.datasets import load_iris
+from sklearn import metrics
 
 import map_classifier
 
 class MapClassifierTest(unittest.TestCase):
 
-  def test_prediction(self):
-    self.assertTrue(True)
+  def test_train_accuracy(self):
+    X, y = load_iris(return_X_y=True)
+    clf = map_classifier.MAPClassifier()
+    clf.fit(X, y)
+    # Predict train set
+    pred_y = clf.predict(X)
+    # Train accuracy
+    train_accuracy = metrics.accuracy_score(y, pred_y)
+    # Train accuracy should > 0.95
+    self.assertGreater(train_accuracy, 0.95)
 
 
 def suite():
