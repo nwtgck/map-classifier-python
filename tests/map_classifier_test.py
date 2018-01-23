1155330cd2c89110d5b04eb47e0d2a9bca31a6d2
Ryo Ota <nwtgck@gmail.com>
2018-01-24 00:10:17 +0900

    [test] Add a k-fold CV test

--- tests/map_classifier_test.py
+++ tests/map_classifier_test.py
@@ -1,8 +1,17 @@
 import unittest
 from sklearn.datasets import load_iris
 from sklearn import metrics
-
+from sklearn import model_selection
+import numpy as np
+import random
 import map_classifier
+
+STANDARD_RANDOM_SEED = 101
+NP_RANDOM_SEED       = 1010
+# Set standard random seed
+random.seed(STANDARD_RANDOM_SEED)
+# Set numpy random seed
+np.random.seed(NP_RANDOM_SEED)
 
 class MapClassifierTest(unittest.TestCase):
 
@@ -11,11 +20,31 @@ class MapClassifierTest(unittest.TestCase):
     clf = map_classifier.MAPClassifier()
     clf.fit(X, y)
     # Predict train set
-    pred_y = clf.predict(X)
+    y_pred = clf.predict(X)
     # Train accuracy
-    train_accuracy = metrics.accuracy_score(y, pred_y)
-    # Train accuracy should > 0.95
-    self.assertGreater(train_accuracy, 0.95)
+    train_accuracy = metrics.accuracy_score(y, y_pred)
+    # Train accuracy should > 0.90
+    self.assertGreater(train_accuracy, 0.90)
+
+  def test_k_fold_cv(self):
+    X, y = load_iris(return_X_y=True)
+    kf = model_selection.KFold(n_splits=5, shuffle=True)
+    clf = map_classifier.MAPClassifier()
+
+    test_accuracies = []
+    for train_index, test_index in  kf.split(X):
+      X_train, X_test = X[train_index], X[test_index]
+      y_train, y_test = y[train_index], y[test_index]
+
+      clf.fit(X_train, y_train)
+      y_pred = clf.predict(X_test)
+
+      test_accuracy = metrics.accuracy_score(y_test, y_pred)
+      test_accuracies.append(test_accuracy)
+
+    avg_test_accuracy = np.mean(test_accuracies)
+    # Average test accuracy should > 0.65
+    self.assertGreater(avg_test_accuracy, 0.65)
 
 
 def suite():
