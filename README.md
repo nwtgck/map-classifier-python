dccbc7b8b197ba9c79cd69626455d3c3ca0b4169
Ryo Ota <nwtgck@gmail.com>
2018-01-24 00:47:46 +0900

    [change] Make the example simpler

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ pip3 install --upgrade git+https://github.com/nwtgck/map-classifier-python.git
 ```python
 from sklearn.datasets import load_iris
 from sklearn import metrics
-from sklearn import model_selection
-import numpy as np
+from sklearn.model_selection import train_test_split
+
 import map_classifier
 
 
@@ -27,29 +27,20 @@ X, y = load_iris(return_X_y=True)
 # Create a classifier
 clf = map_classifier.MAPClassifier()
 
-# K-fold CV
-kf = model_selection.KFold(n_splits=5, shuffle=True)
-test_accuracies = []
-for train_index, test_index in  kf.split(X):
-  # Get training and test sets
-  X_train, X_test = X[train_index], X[test_index]
-  y_train, y_test = y[train_index], y[test_index]
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=40)
 
-  # Learn then model
-  clf.fit(X_train, y_train)
+# Learn then model
+clf.fit(X_train, y_train)
 
-  # Predict
-  y_pred = clf.predict(X_test)
+# Predict
+y_pred = clf.predict(X_test)
 
-  # Calc accuracy
-  test_accuracy = metrics.accuracy_score(y_test, y_pred)
-  test_accuracies.append(test_accuracy)
+# Calc accuracy
+test_accuracy = metrics.accuracy_score(y_test, y_pred)
 
-# Calc average accuracy
-avg_test_accuracy = np.mean(test_accuracies)
-
-# Print the average accuracy
-print(avg_test_accuracy)
+# Print the accuracy
+print(test_accuracy)
 ```
 
 You can find examples in [examples](examples).
--- examples/example1.py
+++ examples/example1.py
@@ -1,16 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn import metrics
-from sklearn import model_selection
-import numpy as np
-import random
-import map_classifier
+from sklearn.model_selection import train_test_split
 
-STANDARD_RANDOM_SEED = 101
-NP_RANDOM_SEED       = 1010
-# Set standard random seed
-random.seed(STANDARD_RANDOM_SEED)
-# Set numpy random seed
-np.random.seed(NP_RANDOM_SEED)
+import map_classifier
 
 
 # Load Iris data set
@@ -19,26 +11,17 @@ X, y = load_iris(return_X_y=True)
 # Create a classifier
 clf = map_classifier.MAPClassifier()
 
-# K-fold CV
-kf = model_selection.KFold(n_splits=5, shuffle=True)
-test_accuracies = []
-for train_index, test_index in  kf.split(X):
-  # Get training and test sets
-  X_train, X_test = X[train_index], X[test_index]
-  y_train, y_test = y[train_index], y[test_index]
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=40)
 
-  # Learn then model
-  clf.fit(X_train, y_train)
+# Learn then model
+clf.fit(X_train, y_train)
 
-  # Predict
-  y_pred = clf.predict(X_test)
+# Predict
+y_pred = clf.predict(X_test)
 
-  # Calc accuracy
-  test_accuracy = metrics.accuracy_score(y_test, y_pred)
-  test_accuracies.append(test_accuracy)
+# Calc accuracy
+test_accuracy = metrics.accuracy_score(y_test, y_pred)
 
-# Calc average accuracy
-avg_test_accuracy = np.mean(test_accuracies)
-
-# Print the average accuracy
-print(avg_test_accuracy)
+# Print the accuracy
+print(test_accuracy)
